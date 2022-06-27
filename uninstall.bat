dff3f80631c3d6ec842b7aa5378d61d1035a9115
seba_mobile <>
2022-06-27 14:03:27 +0200

    added second day

--- uninstall.bat
+++ uninstall.bat
@@ -6,4 +6,5 @@ rd /q /s "C:\Users\Autologin\main_patch\"
 SCHTASKS /DELETE /TN "abistreich_main" /F
 SCHTASKS /DELETE /TN "abistreich_vol" /F
 SCHTASKS /DELETE /TN "abistreich_pic" /F
+SCHTASKS /DELETE /TN "abistreich_sec" /F
 echo "succesfully deleted"
