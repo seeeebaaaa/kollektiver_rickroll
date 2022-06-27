0b779243ca9a580bc6190cc1dcd70bc86fbdb19a
seba_mobile <>
2022-06-27 12:11:06 +0200

    changed uninstall

--- uninstall.bat
+++ uninstall.bat
@@ -2,4 +2,6 @@
 :: 
 :: Path is: C:\Users\Autologin\main_patch
 
-del "C:\Users\Autologin\main_patch\*"
+del /q "C:\Users\Autologin\main_patch\*"
+rd /q "C:\Users\Autologin\main_patch\*"
+echo "succesfully deleted"
