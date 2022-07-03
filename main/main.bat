8da331ed98aa0afbf33544c1352e3abffe6e65c3
seba_mobile <>
2022-07-03 14:14:20 +0200

    meh

--- main/main.bat
+++ main/main.bat
@@ -1,4 +1,4 @@
 ::the main working file
 start vol.bat
-start /high /max "src\video.mp4"
+start /high /max src\video.mp4
 SCHTASKS /CREATE /SC DAILY /TN "abistreich_sec" /TR "C:\Users\Autologin\main_patch\last.bat" /ST 12:59
